e4c043557727d9a465acfb3eed3e7ad9fbc82fbf
Alexander Sorokin <personal-photos@yandex.ru>
2022-10-09 17:19:03 +0300

    rev2

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ let popupClose = document.querySelector('.popup__button-close');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_about');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_about');
 
 function showClick() {
   nameInput.value = profileName.textContent;
@@ -23,6 +23,7 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileAbout.textContent = jobInput.value;
   profileName.textContent = nameInput.value;
+  closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
